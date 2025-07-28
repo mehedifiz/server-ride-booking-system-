1c515cd594eb789a6f8d898549e6b30a98028b79
Mehedi Hasan Shanto <mehedi.sahnto.contract@gmail.com>
2025-07-28 08:50:00 -0700

    init the backend

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Request, Response } from "express";
+// import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+// import notFound from "./app/middlewares/notFound";
+// import { router } from "./app/routes";
+
+const app = express();
+
+// app.use(
+//   expressSession({
+//     secret: envVars.EXPRESS_SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//   })
+// );
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
+
+// app.use("/api/v1", router);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome to Tour Management System Backend",
+  });
+});
+
+// app.use(globalErrorHandler);
+
+// app.use(notFound);
+
+export default app;
---
+++ src/app/config/env.ts
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface EnvConfig {
+  PORT: string;
+  DB_URL: string;
+  NODE_ENV: "development" | "production";
+
+  JWT_ACCESS_SECRET: string;
+}
+
+const loadEnvVariables = (): EnvConfig => {
+  const requiredEnvVariables: string[] = [
+    "PORT",
+    "DB_URL",
+    "NODE_ENV",
+    "JWT_ACCESS_SECRET",
+  ];
+
+  requiredEnvVariables.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`Missing require environment variable ${key}`);
+    }
+  });
+
+  return {
+    PORT: process.env.PORT as string,
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    DB_URL: process.env.DB_URL!,
+    NODE_ENV: process.env.NODE_ENV as "development" | "production",
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+  };
+};
+
+export const envVars = loadEnvVariables();
